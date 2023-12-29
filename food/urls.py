fd85414373a117eb6e6c51f496695077c8d5d455
wkurbatow <wkurbatow@gmail.com>
2023-12-29 20:32:55 +0200

    1

--- food/urls.py
+++ food/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # /food/str
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    path('add/', views.create_item, name='create_item'),
     
 
 ]
--- food/views.py
+++ food/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
+from .forms import ItemForm
 from django.template import loader
+
 
 
 # Create your views here.
@@ -25,3 +27,9 @@ def detail(request, item_id):
 
     return render(request, 'food/detail.html' , context)
 
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid:
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item-form.html', {'form':form} )
