00f1aae4b50abb17fa0c3dd9eb486540cc44dc10
wkurbatow <wkurbatow@gmail.com>
2023-12-29 17:59:12 +0200

    1

--- food/models.py
+++ food/models.py
@@ -8,3 +8,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default = "https://assets.materialup.com/uploads/b03b23aa-aa69-4657-aa5e-fa5fef2c76e8/preview.png")
